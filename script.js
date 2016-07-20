45d53363a0dc24579144187bf60e45036f84e56a
Aleksei <aleksey.lisovich@gmail.com>
2016-07-20 15:31:04 +0300

    task2

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-function createGenerator(){
-	var startNum = document.getElementById("start").value;
-  var stepNum = document.getElementById("step").value;
-  var generator = sequence(+startNum, +stepNum);
+function createGenerator() {
+    var startNum = document.getElementById("start").value;
+    var stepNum = document.getElementById("step").value;
+    var count = document.getElementById("count").value;
+    var generator = sequence(+startNum, +stepNum);
 
-  document.getElementById('calc').onclick = function() {
-  	writeResult(generator);
-  };
+    var resultBlock1 = document.getElementById("resultBlock1");
+    resultBlock1.innerHTML = "Generator is created";
+
+    document.getElementById('calc').onclick = function() {
+        writeResult(generator);
+    };
+
+    document.getElementById('showArray').onclick = function() {
+        var s = take(generator, parseInt(count));
+        var resultBlock2 = document.getElementById("resultBlock2");
+        resultBlock2.innerHTML = s;
+    };
 }
 
 function writeResult(g) {
-    	var resultBlock = document.getElementById("result_block");
-      resultBlock.innerHTML = g();
+    var resultBlock1 = document.getElementById("resultBlock1");
+    resultBlock1.innerHTML = g();
 }
 
 function sequence(start, step) {
-	var currentIteration = 0;
+    var currentIteration = 0;
 
-  return function(){
-		return start + step * currentIteration++;
-	}
+    return function() {
+        return start + step * currentIteration++;
+    }
+}
+
+function take(fn, count) {
+    var array = [];
+    for (var i = 0; i < count; i++) {
+        array[i] = fn();
+    }
+    return array;
 }
